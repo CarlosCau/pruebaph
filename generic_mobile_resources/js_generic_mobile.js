8a2d214aef61bbb5cf32399021336faa088857ba
CarlosCau <43162612+CarlosCau@users.noreply.github.com>
2020-04-10 01:59:50 -0500

    Update js_generic_mobile.js

--- generic_mobile_resources/js_generic_mobile.js
+++ generic_mobile_resources/js_generic_mobile.js
@@ -3,11 +3,50 @@ function openSubScreen(scrn){
     $("#"+scrn).show();
 }
 
+//function randomizePos(e){
+//    var offsetX = Math.random()*0.00500-0.0025;
+//    var offsetY = Math.random()*0.00500-0.0025;
+//    e.latitude=e.latitude+offsetY;
+//    e.longitude=e.longitude+offsetX;
+//    e.latlng=L.latLng([e.latitude, e.longitude]);
+//    return e;
+//}
+
 function randomizePos(e){
-    var offsetX= Math.random()*0.00500-0.0025;
-    var offsetY= Math.random()*0.00500-0.0025;
-    e.latitude=e.latitude+offsetY;
-    e.longitude=e.longitude+offsetX;
-    e.latlng=L.latLng([e.latitude, e.longitude]);
     return e;
+}
+
+function startAutolocate(){
+    $("#btnAutolocate").html("On");
+    clearInterval(intAutolocate);
+    intAutolocate=setInterval(function(){
+        var radius = Math.min(200, posCurrent.accuracy/2)
+        radius = Math.max(10, radius)
+        if (mrkCurrentLocation) {
+            mrkCurrentLocation.remove();
+        }
+        mrkCurrentLocation = L.circle(posCurrent.latlng, {radius:radius}).addTo(mymap);
+        mymap.setView(posCurrent.latlng, 17);
+    }, $("#numAutolocate").val()*1000)
+}
+
+function stopAutolocate(){
+    $("#btnAutolocate").html("Off");
+    clearInterval(intAutolocate);
+}
+
+function startBreadcrumbs(){
+    $("#btnBreadcrumbs").html("On");
+    clearInterval(intBreadcrumbs);
+    intBreadcrumbs=setInterval(function(){
+        var radius = Math.min(200, posCurrent.accuracy/2)
+        radius = Math.max(10, radius)
+        var mrkBreadcrumb = L.circle(posCurrent.latlng, {radius:radius, color:'green'}).addTo(mymap);
+        lyrBreadcrumbs.addLayer(mrkBreadcrumb);
+    }, $("#numBreadcrumbs").val()*1000)
+}
+
+function stopBreadcrumbs(){
+    $("#btnBreadcrumbs").html("Off");
+    clearInterval(intBreadcrumbs);
 }
