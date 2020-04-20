918dff5862c2267d879470adb48f7d0245e83e8c
CarlosCau <43162612+CarlosCau@users.noreply.github.com>
2020-04-19 19:53:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-
+<?php include("../includes/init.php");?>
+<!--<?php 
+    if (logged_in()) {
+        $username=$_SESSION['username'];
+        if (!verify_user_group($pdo, $username, "Guest")) {
+            set_msg("User '{$username}' does not have permission to view this page");
+            redirect('../index.php');
+        }
+    } else {
+        set_msg("Please log-in and try again");
+        redirect('../index.php');
+    } 
+?>-->
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -42,13 +54,17 @@
        <!-- Map screen -->
        
         <div id="divHeader" class="col-xs-12">
-           <span class="pull-left">(<span class="time_since_fix"></span>s)</span>
-           <span class="pull-right">(&plusmn; <span class="info_cur_acc"></span>m)</span>
-            <div class="container p-3 my-3 bg-dark text-white">
+             <div class="pull-left col-xs-3 div-no-padding">(<span class="time_since_fix"></span>s)</div>
+             <div class="col-xs-6 text-center div-no-padding">
+                 <span id="btnStreamStop" class="stream-controls"><i class="fa fa-stop-circle fa-2x"></i></span>
+             </div> 
+             <div class="col-xs-3 div-no-padding"><span class="pull-right">(&plusmn; <span class="info_cur_acc"></span>m)</span></div>
+             <div class="col-xs-12" id="mode" class="text-center"></div>
+             <div class="container p-3 my-3 bg-dark text-white">
               <h3>PERCEPCIÓN LOCAL DEL RIESGO</h3>
               <hr class="my-1">
               <p>Levantamiento de Información en Campo</p>
-            </div>
+             </div>
         </div>
         <div id="divMap" class="col-xs-12">
             <div id="divCross"><i class="fa fa-crosshairs fa-2x"></i></div>
@@ -139,13 +155,87 @@
                 <div class="col-xs-6">
                     <input type="text" class="form-control" name="info_dif_clr" id="info_dif_clr" placeholder="Climbing Rate" readonly>
                 </div>
+                <div id="loginInfo" class="text-center col-xs-12"></div>
             </div>
         </div>
         <div id="divLayers" class="modal">
             <div class="sub-header"><h3 class="text-center">Layers</h3></div>
             <div id="layers">
-                Layer Content
+                <table class="table">
+                    <tr><td>gen_point</td><td><i id="gen_point_collect" class="fa fa-plus"></i></td><td><i id="gen_point_list" class="fa fa-list-alt"></i></td><td><i id="gen_point_refresh" class="fa fa-refresh"></i></td><td><i id="gen_point_download" class="fa fa-cloud-download"></i></td></tr>
+                    <tr><td>gen_line</td><td><i id="gen_line_collect" class="fa fa-plus"></i></td><td><i id="gen_line_list" class="fa fa-list-alt"></i></td><td><i id="gen_line_refresh" class="fa fa-refresh"></i></td><td><i id="gen_line_download" class="fa fa-cloud-download"></i></td></tr>
+                    <tr><td>gen_poly</td><td><i id="gen_poly_collect" class="fa fa-plus"></i></td><td><i id="gen_poly_list" class="fa fa-list-alt"></i></td><td><i id="gen_poly_refresh" class="fa fa-refresh"></i></td><td><i id="gen_poly_download" class="fa fa-cloud-download"></i></td></tr>
+                    <tr><td>COLONIAS</td><td><i id="gen_ao_list" class="fa fa-list-alt"></i></td></tr>
+                </table>
             </div>
+        </div>
+        <div id="divFeatures" class="modal">
+            <div class="sub-header"><h3 id="hdrFeatures" class="text-center">Features</h3></div>
+            <div id="features"></div>
+        </div>
+        <div id="divGenForm" class="modal">
+            <div class="sub-header"><h3 id="hdrGenForm" class="text-center">Features</h3></div>
+            <div id="gen_form">
+               <div class="form-group">
+                    <label class="control-label col-xs-3" for="gen_id">ID:</label>
+                    <div class="col-xs-9">
+                          <input type="text" class="form-control inpGenForm" name="id" id="gen_id" placeholder="ID" readonly>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-xs-3" for="gen_name">Name:</label>
+                    <div class="col-xs-9">
+                          <input type="text" class="form-control inpGenForm" name="name" id="gen_name" placeholder="Name">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-xs-3" for="gen_descr">Comments:</label>
+                    <div class="col-xs-9">
+                          <input type="text" class="form-control inpGenForm" name="descr" id="gen_descr" placeholder="Description">
+                    </div>
+                </div>
+            </div>
+            <div id="gen_form_pt">
+                <div class="form-group">
+                    <label class="control-label col-xs-3" for="gen_lat">Latitude:</label>
+                    <div class="col-xs-9">
+                          <input type="text" class="form-control inpGenForm" name="lat" id="gen_lat" placeholder="Latitude" readonly>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-xs-3" for="gen_lng">Longitude:</label>
+                    <div class="col-xs-9">
+                          <input type="text" class="form-control inpGenForm" name="lng" id="gen_lng" placeholder="Longitude" readonly>
+                    </div>
+                </div>
+                <div id="divAverage" class="col-xs-12 text-center">60s remaining</div>
+                <div class="btn-group btn-group-justified col-xs-12">
+                    <div class="btn-group">
+                        <buttton id="btnGPSPoint" class="btn btn-info btn-block">GPS</buttton>
+                    </div>
+                    <div class="btn-group">
+                        <buttton id="btnGPSAverage" class="btn btn-info btn-block">Average</buttton>
+                    </div>
+                    <div class="btn-group">
+                        <buttton id="btnScreenPoint" class="btn btn-info btn-block">Screen</buttton>
+                    </div>
+                </div>
+
+                
+            </div>
+            <div id="gen_form_ln">
+                <div class="form-group">
+                    <label class="control-label col-xs-3" for="gen_geojson">GeoJSON:</label>
+                    <div class="col-xs-9">
+                          <input type="text" class="form-control inpGenForm" name="geojson" id="gen_geojson" placeholder="GeoJSON">
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12">
+                <buttton id="btnGenFormInsert" class="btn btn-success btn-block">Insert</buttton>
+                <buttton id="btnGenFormUpdate" class="btn btn-warning btn-block">Update</buttton>
+            </div>
+            
         </div>
         <div id="divPoints" class="modal">
             <div class="sub-header"><h3 class="text-center">Points</h3></div>
@@ -161,11 +251,11 @@
                     <button id="btnAutolocate" class="btn btn-warning btn-block">Off</button>
                 </div>
                 <div id="sldrAutolocate" class="col-xs-12">
-                    <div class="col-xs-1">3s</div>
-                    <div class="col-xs-8">
+                    <div class="col-xs-1 div-no-padding">3s</div>
+                    <div class="col-xs-10 div-no-padding">
                         <input id="numAutolocate" type="range" min="3" max="30" step="3" value="9">
                     </div>
-                    <div class="col-xs-2">30s</div>
+                    <div class="col-xs-1 div-no-padding">30s</div>
                 </div>
                 <div class="col-xs-8">
                     <h4 class="setting-label">Breadcrumbs: (<span id="valBreadcrumbs">10</span>s)</h4>
@@ -174,11 +264,11 @@
                     <button id="btnBreadcrumbs" class="btn btn-warning btn-block">Off</button>
                 </div>
                 <div id="sldrBreadcrumbs" class="col-xs-12">
-                    <div class="col-xs-1">5s</div>
-                    <div class="col-xs-8">
+                    <div class="col-xs-1 div-no-padding">5s</div>
+                    <div class="col-xs-10 div-no-padding">
                         <input id="numBreadcrumbs" type="range" min="5" max="60" step="5" value="10">
                     </div>
-                    <div class="col-xs-2">60s</div>
+                    <div class="col-xs-1 div-no-padding">60s</div>
                 </div>
 
                 <div class="col-xs-8">
@@ -188,11 +278,14 @@
                     <button id="btnFilter" class="btn btn-warning btn-block">Off</button>
                 </div>
                 <div id="sldrFilter" class="col-xs-12">
-                    <div class="col-xs-1">5m</div>
-                    <div class="col-xs-8">
+                    <div class="col-xs-1 div-no-padding">5m</div>
+                    <div class="col-xs-10 div-no-padding">
                         <input id="numFilter" type="range" min="5" max="50" step="5" value="30">
                     </div>
-                    <div class="col-xs-2">50m</div>
+                    <div class="col-xs-1 div-no-padding">50m</div>
+                </div>
+                <div class="col-xs-12">
+                    <button id="btnLogout" class="btn btn-danger btn-block">Logout</button>
                 </div>
            
             </div>
@@ -200,17 +293,45 @@
         
     </body>
     <script>
+        var user;
+        $.ajax({
+            url:'php/return_user.php',
+            success:function(response){
+                if (response.substring(0,5)=="ERROR") {
+                    alert(response);
+                } else {
+                    user=JSON.parse(response);
+                    $("#loginInfo").html("Corrent User: "+user.username);
+                    refreshGPt();
+                    refreshGLn();
+                    refreshGPly();
+                    refreshAO();
+                }
+            }
+        })
         var mymap;
         var ctlScale;
         var ctlLayers;
         var ctlMeasure;
+        var lyrSearch;
         var lyrBreadcrumbs;
+        var lyrGPt;
+        var jsnGPt;
+        var lyrGLn;
+        var jsnGLn;
+        var lyrGPly;
+        var jsnGPly;
+        var lyrAO;
+        var jsnAO;
         var objBasemaps;
         var objOverlays;
         var mrkCurrentLocation;
         var posCurrent;
         var posPrevious;
         var posLastTime;
+        var dtAverageFinish;
+        var arAverage;
+        var intAverage;
         var intAutolocate;
         var intBreadcrumbs;
         var intInfo;
@@ -308,6 +429,14 @@
                 console.log(e);
             })
             
+            mymap.on('contextmenu',function(e){
+                if(confirm("are you sure you want to create a generic_point at this location?")){
+                    populateCollect("generic_point");
+                    $("#gen_lat").val(e.latlng.lat.toFixed(6));
+                    $("#gen_lng").val(e.latlng.lng.toFixed(6));
+                }
+            })
+            
             if (localStorage.jsnSettings){
                 var jsnSettings=JSON.parse(localStorage.jsnSettings);
                 $("#btnAutolocate").html(jsnSettings.autolocate);
@@ -349,6 +478,8 @@
             openSubScreen("divSettings");
         })
         
+        // ******* Settings events handlers  
+        
         $("#btnAutolocate").click(function(){
             if ($("#btnAutolocate").html()=="On"){
                 stopAutolocate();
@@ -387,6 +518,107 @@
         $("#numFilter").on("change", function(){
             $("#valFilter").html($("#numFilter").val());
             storeSettings();
+        });
+        
+        $("#btnLogout").click(function(){
+            window.location="../logout.php";
+        });
+        
+        // ******* Layers event handlers
+        
+        // ******** generic_ao event handlers
+        
+        $("#gen_ao_list").click(function(){
+            populateFeaturesAO("ao_colonias_igg");
+        });
+
+        // ******** generic_point event handlers
+        
+        $("#gen_point_collect").click(function(){
+            populateCollect("generic_point");
+        });
+        
+        $("#gen_point_list").click(function(){
+            populateFeatures("generic_point");
+        });
+        
+        $("#gen_point_refresh").click(function(){
+            alert("Refreshing point data for "+user.username);
+            refreshGPt();
+        });
+        
+        $("#gen_point_download").click(function(){
+            alert("Point download not enabled");
+        });
+        
+        $("#btnGPSPoint").click(function(){
+            $("#gen_lat").val(posCurrent.latitude.toFixed(6));
+            $("#gen_lng").val(posCurrent.longitude.toFixed(6));
+        });
+        
+        $("#btnGPSAverage").click(function(){
+            calculateAverage();
+        });
+
+        
+        $("#btnScreenPoint").click(function(){
+            stopAutolocate();
+            openSubScreen();
+            $(".stream-controls").show();
+            $("#mode").html("");
+            $("#btnLayers").attr("disabled",true);
+        })
+        
+        // ******** generic_line event handlers
+        
+        $("#gen_line_collect").click(function(){
+            populateCollect("generic_line");
+        });
+        
+        $("#gen_line_list").click(function(){
+            populateFeatures("generic_line");
+        });
+        
+        $("#gen_line_refresh").click(function(){
+            alert("Refreshing line data for "+user.username);
+            refreshGLn();
+        });
+        
+        $("#gen_line_download").click(function(){
+            alert("Line download not enabled");
+        });
+        
+        // ******** generic_poly event handlers
+        
+        $("#gen_poly_collect").click(function(){
+            populateCollect("generic_poly");
+        });
+        
+        $("#gen_poly_list").click(function(){
+            populateFeatures("generic_poly");
+        });
+        
+        $("#gen_poly_refresh").click(function(){
+            alert("Refreshing poly data for "+user.username);
+            refreshGPly();
+        });
+        
+        $("#gen_poly_download").click(function(){
+            alert("Poly download not enabled");
+        });
+        
+        // *** Generic event handlers
+        $("#btnGenFormInsert").click(function(){
+            insertGenForm($("#hdrGenForm").html());
+        });
+        
+        $("#btnStreamStop").click(function(){
+            $("#mode").html("");
+            $("#gen_lat").val(mymap.getCenter().lat.toFixed(6));
+            $("#gen_lng").val(mymap.getCenter().lng.toFixed(6));
+            openSubScreen("divGenForm");
+            $(".stream-controls").hide();
+            $("#btnLayers").attr("disabled", false);
         });
         
     </script>
